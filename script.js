620caa9b6ec8c3ef182d1d797073cd31b9d5900e
modusi <151635495+modusi@users.noreply.github.com>
2025-04-22 22:05:41 +0400

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ function getQueryParams() {
 }
 
 
+function searchLocation() {
+    var location = document.getElementById('location').value;
+    var geocoder = new google.maps.Geocoder();
+
     geocoder.geocode({ 'address': location }, function (results, status) {
         if (status === 'OK') {
             window.map.setCenter(results[0].geometry.location);
